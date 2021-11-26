030dc2df3bf96de663dd2381658814dd6e212475
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-26 12:07:26 +0000

    adds unit tests for CardGame

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,49 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    
+    def setUp(self):
+        self.card_game = CardGame()
+    
+    @unittest.skip('')
+    def test_check_for_ace_returns_true(self):
+        self.card = Card(1,1)
+        self.assertTrue(self.card_game.check_for_ace(self.card))
+
+    @unittest.skip('')
+    def test_check_for_ace_returns_false(self):
+        self.card = Card(1, 2)
+        self.assertFalse(self.card_game.check_for_ace(self.card))
+
+    @unittest.skip('')
+    def test_highest_card_card1_gt_card2_returns_card1(self):
+        self.card1 = Card(2, 10)
+        self.card2 = Card(0, 4)
+        self.assertEqual(self.card1, self.card_game.highest_card(self.card1, self.card2))
+    
+    @unittest.skip('')
+    def test_highest_card_card1_lt_card2_returns_card2(self):
+        self.card1 = Card(3, 7)
+        self.card2 = Card(0, 9)
+        self.assertEqual(self.card2, self.card_game.highest_card(self.card1, self.card2))
+    
+    @unittest.skip('')
+    def test_highest_card_card1_eq_card2_returns_card2(self):
+        self.card1 = Card(3, 7)
+        self.card2 = Card(0, 7)
+        self.assertEqual(self.card2, self.card_game.highest_card(self.card1, self.card2))
+
+    @unittest.skip('')
+    def test_cards_total_empty_list_returns_zero(self):
+        self.cards = []
+        self.assertEqual(0, self.card_game.cards_total(self.cards))
+    
+    @unittest.skip('')
+    def test_cards_total(self):
+        self.card1 = Card(2, 10)
+        self.card2 = Card(0, 4)
+        self.card3 = Card(3, 7)
+        self.card4 = Card(0, 9)
+        self.cards = [self.card1, self.card2, self.card3, self.card4]
+        self.assertEqual(0, self.card_game.cards_total(self.cards))
